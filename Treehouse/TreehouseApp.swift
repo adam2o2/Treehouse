4e1f68c708cc3999247cb0754b630a579417bd50
Adam <admay202@gmail.com>
2025-03-10 21:12:42 -0500

    Initial Commit

---
+++ Treehouse/TreehouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreehouseApp.swift
+//  Treehouse
+//
+//  Created by Safiya May on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TreehouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
